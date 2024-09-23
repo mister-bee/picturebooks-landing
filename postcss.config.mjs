0997f6947922779f207310e50b782c2ea03423ca
BeechTreeLLC <baboettcher@gmail.com>
2024-09-23 10:27:46 -0700

    config x 10

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,33 +1,8 @@
 // postcss.config.mjs
 
-import postcssImport from "postcss-import";
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-
-/** @type {import('postcss-load-config').Config} */
 export default {
-  plugins: [
-    postcssImport(),
-    tailwindcss(),
-    autoprefixer(),
-    // Add other plugins here
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
-// /** @type {import('postcss-load-config').Config} */
-// const config = {
-//   plugins: {
-//     tailwindcss: {},
-//   },
-// };
-
-// export default config;
-
-// // postcss.config.js
-// module.exports = {
-//   plugins: [
-//     'postcss-import',
-//     'tailwindcss',
-//     'autoprefixer',
-//   ],
-// };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,21 +3,18 @@
 import type { Config } from "tailwindcss";
 import forms from "@tailwindcss/forms";
 import typography from "@tailwindcss/typography";
+import aspectRatio from "@tailwindcss/aspect-ratio";
 
 const config: Config = {
   content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
-  plugins: [forms, typography],
+  plugins: [forms, typography, aspectRatio],
 };
 
 export default config;
